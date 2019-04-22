4d71076b7d24a0a1149845de3627305e4459afcd
Nikki-the-deer <nick-2013@live.com>
2019-04-22 09:14:00 -0500

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is..."
+    resp.write "what?"
+    resp.write "My name is..."
+    resp.write "What?"
+    resp.write "Nikki Eubanks"
     resp.finish
   end
 
